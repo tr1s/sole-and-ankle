5cf2259090b06cbf2750b0c5b41e4e02144f7a2d
Tristan Lawrence <trislaw@gmail.com>
2021-08-17 23:38:09 -0400

    demo

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import SHOES from '../../data';
-import ShoeCard from '../ShoeCard';
+import SHOES from "../../data";
+import ShoeCard from "../ShoeCard";
 
 const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShoeWrapper key={shoe.slug}>
+          <ShoeCard {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: -16px;
+  /* gap: 32px; */
+`;
+const ShoeWrapper = styled.div`
+  min-width: 275px;
+  max-width: 450px;
+  flex: 1;
+  margin: 16px;
+`;
 
 export default ShoeGrid;
